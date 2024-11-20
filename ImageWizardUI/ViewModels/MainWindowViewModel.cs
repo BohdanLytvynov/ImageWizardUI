2c0b8555bb27c0b8175f8ce6906c8d98315335ae
BohdanLytvynov <blitvinov19@gmail.com>
2024-11-21 01:50:36 +0200

    AU-7 Continue creating UI markup

---
+++ ImageWizardUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ImageWizardUI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ ImageWizardUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace ImageWizardUI.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
